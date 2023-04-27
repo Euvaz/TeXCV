145ecf6f7d4b39863fd0bae5ac84b8ee1ca3ad8b
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2023-04-27 14:14:15 -0500

    feat(script): Add logging to stderr

--- gen.sh
+++ gen.sh
@@ -10,6 +10,7 @@ do
     for macro in "$prepend_dir"*.sops.yaml
     do
         merge="./out/merge/$(basename -- $macro .sops.yaml)-$(basename -- $document .tex).tex"
+        echo "[INFO] MERGING: DOCUMENT=$document MACRO=$macro MERGE=$merge" >&2
 
         sops --decrypt -- ./def/*.sops.yaml | yq --unwrapScalar ".macros" > $merge
         sops --decrypt $macro | yq --unwrapScalar ".macros" >> $merge
